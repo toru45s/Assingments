f6e0c38d379ce80eeb8838d93b2b8a9d803a54df
Thiago Teixeira <thsteixeira@gmail.com>
2025-03-11 16:01:05 -0700

    dml update

--- sql/dml.sql
+++ sql/dml.sql
@@ -1,1 +1,11 @@
--- TODO
+INSERT INTO instructors(name, email) VALUES 
+  ('Reza', 'reza@cctb.com'), 
+  ('Washington', 'washington@cctb.com'), 
+  ('Henry', 'henry@cctb.com'), 
+  ('Jivesh', 'jivesh@cctb.com'), 
+  ('Paul', 'paul@cctb.com'), 
+  ('Jhon', 'jhon@cctb.com'),
+  ('Pedro', 'pedro@cctb.com'),
+  ('George', 'george@cctb.com'),
+  ('Ryan', 'ryan@cctb.com'),
+  ('Susan', 'susan@cctb.com');
